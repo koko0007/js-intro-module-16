b16e6424564cdde6667606c718bc11ef8ca73eee
koko0007 <shsuperbkoko@gmail.com>
2021-07-31 01:26:14 +0600

    js

--- variable.js
+++ variable.js
@@ -1,3 +1,5 @@
 var price = 21;
 var age = 23;
 var temparature = 56;
+console.log(age);
+var pass = true;
---
+++ variablename.js
@@ -0,0 +1,6 @@
+// var name = "hero name";
+// console.log(name);
+var eggPrice = 53;
+var onionPrice = 21;
+var totalPrice = eggPrice + onionPrice;
+console.log(totalPrice);
